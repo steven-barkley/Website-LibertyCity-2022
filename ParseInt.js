246958db9bfea0d4574b54f737c04a53b16bef67
Steven Barkley <steven.barkley@outlook.com>
2022-04-01 11:23:52 -0400

    Addded ParseInt and whatJavascriptdoes

---
+++ ParseInt.js
@@ -0,0 +1,24 @@
+
+
+function getAge( inputString ) {
+    //Recieving a string
+    let newChar = inputString[ 0 ]
+    // We want to parse that string 
+    // we wnat to take only the first char 
+    // Add the first char int a new variable 
+    let newInt = newChar + 0
+    // Transform that var from char to int
+    // Pass that value to a new var 
+    // Return that newValue as an integer
+    // Format according
+
+    // I would run the list method on the string ( string.array[0])
+    // newChar = string.array[0]
+    // newInt = newChar.int()
+    // return newInt + "Insert formatting here"
+    return parseInt( "8 years old" )
+}
+
+age = getAge( "Steven" )
+
+console.log( age )
